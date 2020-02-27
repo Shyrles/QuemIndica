f39acf63649e9f5fbb08e3ef61a1a22c5e4b95e9
saramargarido <saramargarido@gmail.com>
2020-02-27 19:33:35 -0300

    header deslogado php

---
+++ header_deslog.php
@@ -0,0 +1,17 @@
+<header>
+        <div class="color">
+            <nav class="navbar navbar-light container ">
+                <a class="navbar-brand " href="#">
+                    <img src="imagens/LOGO.png" width="40" height="40" class="d-inline-block align-top" alt=""> Quem Indica</a>
+
+                <form action="index.html" method="POST">
+                    <div class="d-flex ">
+                        <input type="email" name="email" id="email" placeholder="E-mail" required><br>
+                        <input type="password" name="senha" id="Senha" placeholder="Senha" required><br>
+                        <button type="button" class="btn btn-primary btn-sm ">Login</button>
+                    </div>
+                      </form>
+
+            </nav>
+        </div>
+    </header>
