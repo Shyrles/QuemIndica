6b4a978f8f600b2741fb0ecd2a165663fa9988c2
saramargarido <saramargarido@gmail.com>
2020-02-27 19:37:48 -0300

    header deslogado no sobre

--- header_deslog.php
+++ header_deslog.php
@@ -1,17 +1,31 @@
-<header>
-        <div class="color">
-            <nav class="navbar navbar-light container ">
-                <a class="navbar-brand " href="#">
-                    <img src="imagens/LOGO.png" width="40" height="40" class="d-inline-block align-top" alt=""> Quem Indica</a>
+<header class="navbar navbar-expand flex-column flex-md-row bd-navbar fixed-top">
 
-                <form action="index.html" method="POST">
-                    <div class="d-flex ">
-                        <input type="email" name="email" id="email" placeholder="E-mail" required><br>
-                        <input type="password" name="senha" id="Senha" placeholder="Senha" required><br>
-                        <button type="button" class="btn btn-primary btn-sm ">Login</button>
-                    </div>
-                      </form>
+        <!-- Logo esquerda -->
+    <a class="navbar-brand mr-0 mr-md-2" href="#" aria-label="Quem Indica"><img src="imagens/logo_icon.svg" width="40" height="40" 
+            class="d-inline-block align-top" alt=""></a>
+      
+      <!-- Barra de busca -->
+      <div id="divBusca">
+        <input type="text" id="txtBusca" placeholder="Buscar..."/>
+        <a href="#"><img src="imagens/loupe.png" id="btnBusca" alt="Buscar" /></a>
+      </div>
+           
+      <!-- Botões direita -->
+        <ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex">
+            <li class="nav-item">
+              <a class="nav-link active" href="#"><button type="button" class="btn btn-dark">Registrar</button></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#"><button type="button" class="btn btn-info">Entrar</button></a>
+            </li>
+           
+        </ul>
 
-            </nav>
-        </div>
-    </header>
+        <div class="dropdown">
+            
+            <a class="hamburguer" href="#" aria-label="Quem Indica"><img src="imagens/icones_dropdown.svg" width="30" height="30" 
+                class="d-inline-block align-top" alt=""></a>
+        
+            </div>
+
+      </header>
--- index.php
+++ index.php
@@ -14,7 +14,23 @@
 </head>
 
 <body>
-    <?= require_once("header_deslog.php"); ?>
+    <header>
+        <div class="color">
+            <nav class="navbar navbar-light container ">
+                <a class="navbar-brand " href="#">
+                    <img src="imagens/LOGO.png" width="40" height="40" class="d-inline-block align-top" alt=""> Quem Indica</a>
+
+                <form action="index.html" method="POST">
+                    <div class="d-flex ">
+                        <input type="email" name="email" id="email" placeholder="E-mail" required><br>
+                        <input type="password" name="senha" id="Senha" placeholder="Senha" required><br>
+                        <button type="button" class="btn btn-primary btn-sm ">Login</button>
+                    </div>
+                      </form>
+
+            </nav>
+        </div>
+    </header>
     <section>
         <div class="container">
             <div class="row banner">
