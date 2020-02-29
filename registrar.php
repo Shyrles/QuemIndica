4893dee339126e0acbf5698ed44a28667c2bc76a
sandrazfd <56775340+sandrazfd@users.noreply.github.com>
2020-02-29 12:37:42 -0300

    registrar e login

---
+++ registrar.php
@@ -0,0 +1,164 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <title>REGISTRAR</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="shortcut icon" href="imagens/logo_icon.svg" type="image/x-icon">
+</head>
+
+<body>
+    
+    <?= require_once ("header_deslog.php"); ?>
+
+    <section class="banner_azul">
+        <div class="container text-center">
+        <img src="imagens/logo_icon.svg" class="rounded" id="quemindica" alt="Quem Indica">
+        <h1><strong>Registrar</strong></h1>
+        </div>
+    </section>
+
+    <section class="banner_conteudo">
+        <div class="container">
+           
+        <span class="text-justify" id="registrar">
+
+                 <form action="registro.php">
+                    <div id="nome"> <br> <br>
+                            <label for="nome" >Nome</label><br>
+                            <input type="text" name="nome" size="50px" id="nome"  placeholder="Seu nome">
+                          
+                            <p class="obrigatorio">Obrigatório</p>
+                          <br>  
+                    </div>
+                    
+                    <div id="email">
+                        <label for="email" >Email</label><br>
+                        <input type="email" name="email" size="50px" id="email"  placeholder="email@email.com">
+                     
+                        <p class="obrigatorio">Obrigatório</p>
+                          <br>               
+                    </div>   
+
+
+                  <div id="senha">
+                     <label for="senha">Senha</label><br>
+                     <input type="password" name="senha" size="50px" id="senha" placeholder="senha">
+                     <br>
+                     <p class="obrigatorio">Obrigatório</p>
+                     <br>
+                  </div>
+               
+                  <div>
+                    <label for="identifica">Como você se identifica?</label> <br>
+
+                    <div>
+                            <label for="masculino">
+                            <input type="radio" name="genero" id="masculino" value="masculino">masculino
+                            </label>
+
+                            <label for="feminino">
+                            <input type="radio" name="genero" id="feminino" value="feminino">
+                            Feminino</label>
+
+                            <label for="outro">
+                            <input type="radio" name="genero" id="outro" value="outro">
+                            Especificar outro</label>
+                    </div>
+      
+                  </div>
+
+             
+                 <label for="comentarios" id="comentarios" name="comentarios">Comentários </label> <br>
+                 <textarea name="comentarios" id="" cols="50" rows="5"></textarea>
+
+                 <div>
+                     <label for="termos"><input type="checkbox" name="termos" id="termos" value="termod">Aceita os termos e condições</label>
+                 </div>
+     
+        </span>
+        
+        
+        </div>
+    </section>
+
+<!-- 
+    <footer>
+        <div class="rodape">
+            <div class="container d-flex">
+                <ul class="nav flex-inline">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Sobre Nós</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Ajuda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Termos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="#">Política de Dados</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="#">Política de Cookies</a>
+                    </li>
+
+                </ul>
+
+            </div>
+
+        </div>
+
+
+    </footer> -->
+    <footer>
+        <div class="rodape">
+        <!-- esquerda -->
+        
+        <div class="container d-flex">
+
+            <ul class="nav flex-inline">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Copyright © 2020</a>
+                  </li>
+
+            </ul>
+       
+      <!-- direita -->
+        <ul class="flex-row ml-md-auto d-none d-md-flex">
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Sobre Nós</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Ajuda</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Termos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link " href="#">Política de Dados</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link " href="#">Política de Cookies</a>
+            </li>       
+                      
+        </ul>
+
+        </div>
+       
+      </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
