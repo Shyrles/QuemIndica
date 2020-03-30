dbea36a75bafca5635d97928382f9feb5c622beb
saramargarido <saramargarido@gmail.com>
2020-03-30 15:10:42 -0300

    ajuste scrool horizontal

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="css/estilo.css">
     <link rel="stylesheet" href="css/index.css">
     <link rel="shortcut icon" href="imagens/logo_icon.svg" type="image/x-icon">
@@ -20,7 +20,7 @@
     
     <section class="d-flex banner_top">
         
-        <div class="container d-flex align-content-around flex-wrap ">
+        <div class="container-fluid d-flex align-content-around flex-wrap ">
             <div class= "row">
                 <div class="col-6">
                     <img src="imagens/services.png" class="service" alt="">
@@ -67,7 +67,7 @@
 
     <section class="d-flex banner_middle">
 
-         <div class="container d-flex align-content-around flex-wrap">
+         <div class="container-fluid d-flex align-content-around flex-wrap">
             <div class= "row">
                 <div class="col-6 d-flex align-content-around flex-wrap">
                     <h4 class="text-left">O que indica?</h4>
@@ -85,7 +85,7 @@
 
     <section class="d-flex banner_team">
 
-        <div class="container">
+        <div class="container-fluid">
             <div class= "row">
                 <div class="col-6">
                 <img src="imagens/team.png" class="team" alt="">
