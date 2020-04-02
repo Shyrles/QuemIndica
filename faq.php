886a322b340d653b12ea55ce4ead9c608537704f
saramargarido <saramargarido@gmail.com>
2020-04-02 16:41:48 -0300

    faq responsivo

--- faq.php
+++ faq.php
@@ -10,6 +10,7 @@
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="css/faq.css">
     <link rel="shortcut icon" href="imagens/logo_icon.svg" type="image/x-icon">
 </head>
 
@@ -17,20 +18,19 @@
     
     <?php require_once ("header_deslog.php"); ?>
 
-    <section class="banner_cinza">
-
-    <div class="d-flex align-items-center">
-        <img src="imagens/hashtag-01.png" class="rounded" id="hashtag" alt="Quem Indica">
-        <h1 class="d-inline"><strong>faq</strong></h1>
+    <section class="banner_azul">
+        <div class="container text-center">
+        <img src="imagens/logo_icon.svg" class="rounded" id="quemindica" alt="Quem Indica">
+        <h1><strong>FAQ</strong></h1>
         </div>
-
     </section>
 
     <section class="banner_faq">
-        <div class="container">
+        <div class="container questions_box">
+          <h3 class="h3_title"><strong>Perguntas Frequentes</strong></h3>
         <div class='question'>
             <input type='checkbox' id='question-1'>
-            <label for='question-1' class="perguntas">Qual sua pergunta?</label>
+            <label for='question-1'>Qual sua pergunta?</label>
             <div class='answer'>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis officiis id, ratione, excepturi sint, vero repellendus quasi 
               voluptas enim aspernatur amet odit sunt iure laudantium dolores incidunt eaque delectus quam. Lorem ipsum dolor sit amet 
