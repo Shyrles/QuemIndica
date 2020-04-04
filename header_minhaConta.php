d1210a38ffa99024428eb46a459a054b6ee1e4f2
sandrazfd <56775340+sandrazfd@users.noreply.github.com>
2020-04-04 14:06:29 -0300

    responsivo

--- header_minhaConta.php
+++ header_minhaConta.php
@@ -1,38 +1,41 @@
-<header class="navbar navbar-expand flex-column flex-md-row bd-navbar fixed-top fundo">
+<header class="navbar navbar-expand bd-navbar fixed-top fundo">
 
-        <!-- Logo esquerda -->
-    <a class="navbar-brand mr-0 mr-md-2" href="#" aria-label="Quem Indica"><img src="imagens/logo_icon.svg" width="50" height="50" 
-            class="d-inline-block align-top" alt=""></a>
-      
-      <!-- Barra de busca -->
-      
-      <div id="divBusca">
-        <input type="text" id="txtBusca" placeholder="Buscar..."/>
-      </div>
-        
-      <!-- Botões direita -->
-        <ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex">
-            <li class="nav-item">
-                <img id="fotoUsuario" src="imagens/minhaConta/fotoUsuario.jpg" alt="Foto do usuario" class="rounded">
-                <!--a class="nav-link active" href="#"><button type="button" class="btn btn-dark">Registrar</button></a-->
-            </li>
-            
+<!-- Logo esquerda -->
+<img src="imagens/logo_icon.svg" width="50" height="50" >
+   
 
-            <div class="dropdown">
-                <a class="btn btn-info dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Shyrles Monteiro
-                </a>
+<!-- Barra de busca -->
 
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                    <a class="dropdown-item" href="feed.php">Feed</a>
-                    <a class="dropdown-item" href="servicos.php">Serviços</a>
-                    <a class="dropdown-item" href="minha_conta.php">Meu Perfil</a>
-                    <a class="dropdown-item" href="faq.php">FAQ</a>
-                    <a class="dropdown-item" href="sobre.php">Sobre nós</a>
-                    <a class="dropdown-item" href="index.php">Sair</a>          
-                </div>
-            </div>
-     
-        </ul>
+<div id="divBusca">
+<form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
+  
+  </form>
+</div>
 
-        </header>
+<!-- Botões direita -->
+<ul class="navbar-nav flex-row ml-md-auto d-flex">
+    <li class="nav-item">
+        <img id="fotoUsuario" src="imagens/fotoUsuario.jpg" alt="Foto do usuario" width="40" height="40" class="rounded">
+        <!--a class="nav-link active" href="#"><button type="button" class="btn btn-dark">Registrar</button></a-->
+    </li>
+    
+
+    <div class="dropdown">
+        <a class="btn btn-info dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Shyrles Monteiro
+        </a>
+
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            <a class="dropdown-item" href="feed.php">Feed</a>
+            <a class="dropdown-item" href="servicos.php">Serviços</a>
+            <a class="dropdown-item" href="minha_conta.php">Meu Perfil</a>
+            <a class="dropdown-item" href="faq.php">FAQ</a>
+            <a class="dropdown-item" href="sobre.php">Sobre nós</a>
+            <a class="dropdown-item" href="index.php">Sair</a>          
+        </div>
+    </div>
+
+</ul>
+
+</header>
