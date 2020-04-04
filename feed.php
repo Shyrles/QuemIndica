90c558c4cf42eb42ab58469a0e6953854ec13fae
Millena Moschella <millenamoschella@gmail.com>
2020-04-04 11:27:50 -0300

    Página suporte responsiva

--- feed.php
+++ feed.php
@@ -5,37 +5,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Feed - Quem Indica</title>
 
+<!-- área css -->
     <link rel="stylesheet" href="css/feed.css">
     <link rel="stylesheet" href="css/estilo.css">
     <link rel="stylesheet" href="css/footer.css">
     <link rel="stylesheet" href="css/minhaConta.css">
+    <!-- <link rel="stylesheet" href="css/feedresponsivo.css"> -->
+
+<!-- área bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
-        crossorigin="anonymous">
-
+<!-- área ícones -->
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"crossorigin="anonymous">
     <link rel="shortcut icon" href="imagens/logo_icon.svg" type="image/x-icon">
 </head>
+
 <body>
 
+<!-- HEADER -->
 
 
-    <!-- HEADER -->
-
-    <?php require_once ("header_minhaConta.php"); ?>
     
-    <div class="grid-container">
-<!-- ASIDE ESQUERDO-->
+    
+<div class="grid-container">
+
+<header>
+    <?php require_once ("header_minhaConta.php"); ?>
+</header>
+
+
+
+
+<!-- ASIDE ESQUERDO -->
 
     <aside class="left">
-        <div id="usuario">
-        <img src="imagens/usuario.png" alt="foto teste usuário" id="sombra">
-        </div>
+            <div id="avatar-usuario">
+            <img src="imagens/usuario.png" alt="foto teste usuário">
+            </div>
 
-            <div class="row">
-                <div class="col-3">
-                <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+
+            <div class="menu-lateral">
+                <div class="coluna">
+                <div class="nav nav-pills" id="v-pills-tab" role="tablist">
                 <a class="nav-link active" id="v-pills-feed-tab" data-toggle="pill" href="feed.php" role="tab" aria-controls="v-pills-feed" aria-selected="true">Feed</a>
                 <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="minha_conta.php" role="tab" aria-controls="v-pills-profile" aria-selected="false">Perfil</a>
                 <a class="nav-link" id="v-pills-amigos-tab" data-toggle="pill" href="#" role="tab" aria-controls="v-pills-amigos" aria-selected="false">Amigos</a>
@@ -45,158 +57,157 @@
                 <a class="nav-link" id="v-pills-parceiros-tab" data-toggle="pill" href="#" role="tab" aria-controls="v-pills-parceiros" aria-selected="false">Parceiros</a>
                 </div>
                 </div>
-            </div>
 
-                <div class="col-9">
-                    <div class="tab-content" id="v-pills-tabContent">
-                        <div class="tab-pane fade show active" id="v-pills-feed" role="tabpanel" aria-labelledby="v-pills-feed-tab"></div>
-                        <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab"></div>
-                        <div class="tab-pane fade" id="v-pills-amigos" role="tabpanel" aria-labelledby="v-pills-amigos-tab"></div>
-                        <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab"></div>
-                        <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab"></div>
-                        <div class="tab-pane fade" id="v-pills-indicaces" role="tabpanel" aria-labelledby="v-pills-indicacoes-tab"></div>
-                        <div class="tab-pane fade" id="v-pills-parceiros" role="tabpanel" aria-labelledby="v-pills-parceiros-tab"></div>
-                    </div>
+                <div class="coluna">
+                <div class="tab-content" id="v-pills-tabContent">
+                <div class="tab-pane fade show active" id="v-pills-feed" role="tabpanel" aria-labelledby="v-pills-feed-tab"></div>
+                <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab"></div>
+                <div class="tab-pane fade" id="v-pills-amigos" role="tabpanel" aria-labelledby="v-pills-amigos-tab"></div>
+                <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab"></div>
+                <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab"></div>
+                <div class="tab-pane fade" id="v-pills-indicaces" role="tabpanel" aria-labelledby="v-pills-indicacoes-tab"></div>
+                <div class="tab-pane fade" id="v-pills-parceiros" role="tabpanel" aria-labelledby="v-pills-parceiros-tab"></div>
                 </div>
-
-            <div id="explorar">
-            <h4>Hashtags</h4>
-                <ul class="nav flex-column">
+                </div>
+            </div>
+            
+            <div id="explorar-hashtags">
+                <h4>Hashtags</h4>
+                    <ul class="nav">
                     <li class="nav-item"><a class="nav-link" href="#">#BELEZA</a></li>
                     <li class="nav-item"><a class="nav-link" href="#">#LIVROS</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">#FILMES</a></a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">#FILMES</a></li>
                     <li class="nav-item"><a class="nav-link" href="#">#RESTAURANTE</a></li>
-                </ul>               
+                    </ul>               
             </div> 
     </aside>
 
 <!-- ASIDE DIREITO -->
 
     <aside class="right">
-                
-                <form class="form-inline">
+                <div class="busca">
+                    <form>
                     <div class="input-group">
-                            <input type="text" class="form-control" aria-label="Recipient's username" aria-describedby="button-addon2" placeholder="Pesquisar">
-                        <div class="input-group-append">
-                            <button class="btn btn-outline-primary" type="button" id="button-addon2">
-                                <i class="fa fa-search"></i>
-                            </button>
-                        </div>
+                    <input type="text" class="form-control" aria-label="Recipient's username" aria-describedby="button-addon2" placeholder="Pesquisar">
+                    <div class="input-group-append">
+                    <button class="btn btn-outline-primary" type="button" id="button-addon2"><i class="fa fa-search"></i></button>
                     </div>
-                </form>
+                    </div>
+                    </form>
+                </div>
+                
 
                 <div id="cards-servicos">
-                <h4>Serviços</h4>
+                    <h4>Serviços</h4>
+
+                    <div class="cards-servicos">
 
                     <div class="card-1">
-                        <img src="imagens/card-teste-servico.jpg" alt="Imagem de capa do card" class="img-thumbnail img-fluid">
+                    <img src="imagens/card-teste-servico.jpg" alt="Imagem de capa do card" class="img-thumbnail">
                     </div>
 
                     <div class="card-2">
-                        <img src="imagens/card-teste-servico.jpg" alt="Imagem de capa do card" class="img-thumbnail img-fluid">
+                    <img src="imagens/card-teste-servico.jpg" alt="Imagem de capa do card" class="img-thumbnail">
                     </div>
 
                     <div class="card-3">
-                        <img src="imagens/card-teste-servico.jpg" alt="Imagem de capa do card" class="img-thumbnail img-fluid">
+                    <img src="imagens/card-teste-servico.jpg" alt="Imagem de capa do card" class="img-thumbnail">
+                    </div>
+
                     </div>
                 </div>
     </aside>
 
-<!-- CONTEUDO -->
+<!-- ESCREVER PUBLICAÇÃO -->
 
-            <main class="main">
+        <main class="main">
+                <div class="container-fluid gedf-wrapper">
 
-            <div class="container-fluid gedf-wrapper">
-                <div class="row">
+                        <div class="row">
 
-                    <div class="card-body">
-
-                        <div class="tab-content" id="myTabContent">
-                            <div class="tab-pane fade show active" id="posts" role="tabpanel" aria-labelledby="posts-tab">
-                                <div class="form-group">
-                                    <label class="sr-only" for="message">post</label>
-                                    <textarea class="form-control" id="message" rows="3" placeholder="Escreva a sua indicação"></textarea>
+                            <div class="card-body">
+                                <div class="tab-content" id="myTabContent">
+                                    <div class="tab-pane fade show active" id="posts" role="tabpanel" aria-labelledby="posts-tab">
+                                        <div class="form-group">
+                                        <label class="sr-only" for="message">post</label>
+                                        <textarea class="form-control" id="message" rows="3" placeholder="Escreva a sua indicação"></textarea>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-
-                        <div class="btn-toolbar justify-content-between">
-                            <div class="btn-group">
-                                <button type="submit" class="btn btn-primary">compartilhar</button>
-                            </div>
-                            <div class="btn-group">
-                                <button id="btnGroupDrop1" type="button" class="btn btn-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-globe"></i></button>
-                            </div>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="btnGroupDrop1">
-                                <a class="dropdown-item" href="#"><i class="fa fa-globe"></i>Público</a>
-                                <a class="dropdown-item" href="#"><i class="fa fa-users"></i>Amigos</a>
-                            </div>
-                        </div>
-
-                    </div>
-
-                <!-- EXEMPLO POSTAGEM -->
-                <div class="card gedf-card">
-                    <div class="card-header">
-                        <div class="d-flex justify-content-between align-items-center">
-                            <div class="d-flex justify-content-between align-items-center">
-
-                                <div class="mr-2">
-                                    <img class="rounded-circle" width="45" src="../quem-indica/imagens/usuario.png" alt="">
-                                </div>
-                                <div class="ml-2">
-                                    <div class="h5 m-0">@usuário</div>
-                                    <div class="h7 text-muted">Nome do Usuário</div>
-                                </div>
-
-                            </div>
-                        <div>        
-                    </div>
-                </div>
-            </div>
-                    <div class="card-body">
-                        <div class="text-muted h7 mb-2"> <i class="fa fa-clock-o"></i> data e hora da postagem</div>
-
-                            <a class="card-link" href="#">
-                            <h5 class="card-title"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit consectetur
-                                deserunt illo esse distinctio.
-                            </h5>
-                            </a>
-
-                            <p class="card-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam omnis nihil, aliquam est, voluptates officiis iure soluta
-                            alias vel odit, placeat reiciendis ut libero! Quas aliquid natus cumque quae repellendus. Lorem
-                            ipsum dolor sit amet consectetur adipisicing elit. Ipsa, excepturi. Doloremque, reprehenderit!
-                            Quos in maiores, soluta doloremque molestiae reiciendis libero expedita assumenda fuga quae.
-                            Consectetur id molestias itaque facere? Hic!
-                            </p>
-
-                            <div class="hashtags">
-                                <ul>
-								    <li><a href="#" title="">#Filmes</a></li>
-								    <li><a href="#" title="">#Netflix</a></li>
-								    <li><a href="#" title="">#Romance</a></li>
-								    <li><a href="#" title="">#Oscar</a></li>
-                                </ul>
-                            </div>
-                            </div>
+                    
+                                <div class="btn-toolbar justify-content-between">
+                                    <div class="btn-group">
+                                    <button type="submit" class="btn btn-primary">compartilhar</button>
+                                    </div>
                         
+                                    <div class="btn-group">
+                                    <button id="btnGroupDrop1" type="button" class="btn btn-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-globe"></i></button>
+                                    </div>
 
-                            <div class="card-footer">
-                                <a href="#" class="card-link"><i class="fa fa-gittip"></i> Curtir</a>
-                                <a href="#" class="card-link"><i class="fa fa-comment"></i> Comentar</a>
-                                <a href="#" class="card-link"><i class="fa fa-mail-forward"></i> Compartilhar indicação</a>
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="btnGroupDrop1">
+                                    <a class="dropdown-item" href="#"><i class="fa fa-globe"></i>Público</a>
+                                    <a class="dropdown-item" href="#"><i class="fa fa-users"></i>Amigos</a>
+                                    </div>
+                                </div>
                             </div>
-                    </div>
-                </div>  
+
+<!-- EXEMPLO POSTAGEM -->
+                
+                            <div class="card gedf-card">
+
+                                <div class="card-header">
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <div class="d-flex justify-content-between align-items-center">
+                                                <div class="mr-2"><img class="rounded-circle" width="45" src="../quem-indica/imagens/usuario.png" alt="">
+                                                </div>
+                                            <div class="ml-2">
+                                                <div class="h5 m-0">@usuário
+                                                </div>
+
+                                                <div class="h7 text-muted">Nome do Usuário
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            <div class="card-body">
+                                <div class="text-muted h7 mb-2"> <i class="fa fa-clock-o"></i> 10:30
+                                    </div>
+                                        <a class="card-link" href="#"><h5 class="card-title">Título postagem</h5></a>
+                                        <p class="card-text">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam omnis nihil, aliquam est, 
+                                        voluptates officiis iure soluta
+                                        alias vel odit, placeat reiciendis ut libero! Quas aliquid natus cumque quae repellendus. Lorem
+                                        ipsum dolor sit amet consectetur adipisicing elit. Ipsa, excepturi. Doloremque, reprehenderit!
+                                        Quos in maiores, soluta doloremque molestiae reiciendis libero expedita assumenda fuga quae.
+                                        Consectetur id molestias itaque facere? Hic!
+                                        </p>
+                                    </div>
+
+                                    <div class="card-footer">
+                                        <a href="#" class="card-link"><i class="fa fa-gittip"></i> Curtir</a>
+                                        <a href="#" class="card-link"><i class="fa fa-comment"></i> Comentar</a>
+                                        <a href="#" class="card-link"><i class="fa fa-mail-forward"></i> Compartilhar indicação</a>
+                                    </div>
+                                </div>
+                            </div>
+                    
+                        </div>
+                </div>
         </main>
-    
-</div>
+
+<!-- FOOTER -->
+        <footer>
             <?php require_once ("footer.php");?>
 
+        </footer>
 
-    
+            
+</div> <!--fechamento: div.container -->
+
+
+<!-- ÁREA SCRIPTS -->
 
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8=" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
--- suporte.php
+++ suporte.php
@@ -1,46 +1,108 @@
 <!DOCTYPE html>
-    <html lang="pt-br">
-        <head>
-            <meta charset="UTF-8">
-            <title>Suporte*</title>
-            <link rel="stylesheet" href="css/suporte.css">
-            <link rel="shortcut icon" href="imagens/logo-quem-indica.png" type="image/x-icon">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        </head>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Feed - Quem Indica</title>
 
-        <body>
+<!-- área css -->
+    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="css/suporte.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/minhaConta.css">
+
+<!-- área bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+
+
+</head>
+<body>
+
+<!-- HEADER -->
+
+
+    
+    
+<div class="grid-container">
+
         <header>
-
-
+            <?php require_once ("header_minhaConta.php"); ?>
         </header>
 
 
-<main class="container">
-    <h1>Suporte</h1>
 
-    <form action="contato.html" method="post">
+<!-- FORMULARIO -->
 
-        <div class="nome"><label for="nome"></label><br>
-            <input type="text" name="nomecontato" id="Nome" placeholder="Nome*" required></div>
+        <!-- <main class="main"> -->
 
-        <div class="sobrenome"><label for="sobrenome"></label><br>
-            <input type="text" name="sobrenomecontato" id="Sobrenome" placeholder="Sobrenome*" required></div>
-
-        <div class="email"><label for="email"></label><br>
-            <input type="email" name="email" id="email" placeholder="Email*" required></div>
-
-        <div class="telefone"><label for="telefone"></label><br>
-            <input type="tel" name="telefone" id="telefone" placeholder="Telefone*" maxlength="11" required></div>
-            
-        <div class="mensagem"><textarea name="mensagem" id="mensagem" cols="50" rows="10" placeholder="Mensagem*" required></textarea></div>
-
-        <button type="submit">Enviar</button>
-    </form>        
-</main>
-        <footer>
-
-        </footer>
     
-</body>
 
-    </html>
+<div class="main">
+        <div class="container">
+            <div class="signup-content">
+
+                <div class="signup-img">
+                    <img src="../QuemIndica-master/imagens/banner02.jpg" alt="">
+                </div>
+
+                <div class="signup-form">
+                    <form action="suporte.php" method="POST" class="register-form" id="register-form">
+
+                        <div class="titulo">
+                            <h2>Suporte</h2>
+                        </div>
+
+                        <div class="row">
+                            <div class="col">
+                            <label for="nome">Nome</label>
+                            <input type="text" value="" name="nome" id="nome" class="form-control" placeholder="Nome">
+                        </div>
+
+                        <div class="col">
+                            <label for="sobrenome">Sobrenome</label>
+                            <input type="text" value="" name="sobrenome" id="sobrenome" class="form-control" placeholder="Sobrenome">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input class="form-control" type="text" value="" name="email" id="email" placeholder="@.com">
+                        </div>
+
+                        <div class="form-group purple-border">
+                            <label for="exampleFormControlTextarea4">Mensagem</label>
+                            <textarea class="form-control" name="mensagem" id="email" rows="3"></textarea>
+                        </div>
+                        
+                        <div class="input-group-append" id="enviar">
+                            <button class="btn btn-md btn-dark m-0 px-3" type="button" value="" class="submit" name="enviar" id="enviar">Enviar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+            
+</div> <!--fechamento: div.container -->
+
+
+<!-- FOOTER -->
+        <footer>
+            <?php require_once ("footer.php");?>
+        </footer>
+
+
+<!-- ÁREA SCRIPTS -->
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8=" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+
+</body>
+</html>
