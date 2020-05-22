e4639d69101047bf114260add0e4b80efc97b97a
saramargarido <saramargarido@gmail.com>
2020-05-22 13:22:23 -0300

    voltando sem modal

--- header_deslog.php
+++ header_deslog.php
@@ -1,81 +1,4 @@
-<header>
-
-<div class="fixed-top">
-
-
-
-  <!-- Trigger the modal with a button -->
-  <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">instruction</button>
-  </div>
-  
-  <!-- Modal -->
-  <div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog">
-    
-      <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">Modal Header</h4>
-        </div>
-        <div class="modal-body">
-          <p>Some text in the modal.</p>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        </div>
-      </div>
-      
-    </div>
-
-    <nav class="navbar navbar-expand-lg navbar-light  ml-auto " id="btn_qi">
-  
-  <a class="navbar-brand " href="#">
-    <img src="imagens/logo_icon.svg" width="50" height="50" alt="">
-   <strong>Quem Indica</strong> 
-  </a>
-  
-  
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="index.php"><img src="icones/home1.png" width="35" height="35" alt = "home"></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="suporte.php"><img src="icones/suporte.png" width="50" height="50" alt = "suporte"></a>
-      </li>
-     
-      </ul>
-  </div>
-
-  <div class= "dropdown">
-      <img src="imagens/usuario.png"  width="40" height="40" alt="">
-    
-      <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" 
-      aria-expanded="false">
-      <strong id="bemvindo">Bem-vindo</strong>
-      </a>
-
-  <div class="dropdown" id="cores">
-      
-    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-      <a class="dropdown-item"  href="login.php">Entrar</a>
-      <a class="dropdown-item"  href="registrar.php">Criar Conta</a>
-    </div>
-  </div>
-</div>
-
-
-
-        
-  
-</nav>
-  </div>
-
-  
-
-</header>
-<!-- <header class="container-fluid fixed-top">     
+<header class="container-fluid fixed-top">     
 
 <div class="pos-f-t">
  
@@ -123,5 +46,5 @@
 </nav>
 
 
-      </header> -->
+      </header>
       
